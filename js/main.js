50b9e7f0bf012e6a79d8d15f0db311fc53ef124a
birobalint95 <87605943+birobalint95@users.noreply.github.com>
2021-08-23 13:21:31 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@
 
 // document.getElementById("typewriter").innerHTML = "HELLO";
 
+/*
+
 console.log("szia");
 
 function hello() {
@@ -13,80 +15,76 @@ console.log(typer);
 
 hello();
 
+*/
 
+function setupTypewriter(t) {
+  var HTML = t.innerHTML;
 
-// console.log(typer);
+  t.innerHTML = "";
 
-/*
-  function setupTypewriter(t) {
-      var HTML = t.innerHTML;
+  var cursorPosition = 0,
+      tag = "",
+      writingTag = false,
+      tagOpen = false,
+      typeSpeed = 100,
+    tempTypeSpeed = 0;
 
-      t.innerHTML = "";
+  var type = function() {
 
-      var cursorPosition = 0,
-          tag = "",
-          writingTag = false,
-          tagOpen = false,
-          typeSpeed = 100,
-        tempTypeSpeed = 0;
+      if (writingTag === true) {
+          tag += HTML[cursorPosition];
+      }
 
-      var type = function() {
-        
-          if (writingTag === true) {
+      if (HTML[cursorPosition] === "<") {
+          tempTypeSpeed = 0;
+          if (tagOpen) {
+              tagOpen = false;
+              writingTag = true;
+          } else {
+              tag = "";
+              tagOpen = true;
+              writingTag = true;
               tag += HTML[cursorPosition];
           }
-
-          if (HTML[cursorPosition] === "<") {
+      }
+      if (!writingTag && tagOpen) {
+          tag.innerHTML += HTML[cursorPosition];
+      }
+      if (!writingTag && !tagOpen) {
+          if (HTML[cursorPosition] === " ") {
               tempTypeSpeed = 0;
-              if (tagOpen) {
-                  tagOpen = false;
-                  writingTag = true;
-              } else {
-                  tag = "";
-                  tagOpen = true;
-                  writingTag = true;
-                  tag += HTML[cursorPosition];
-              }
           }
-          if (!writingTag && tagOpen) {
-              tag.innerHTML += HTML[cursorPosition];
-          }
-          if (!writingTag && !tagOpen) {
-              if (HTML[cursorPosition] === " ") {
-                  tempTypeSpeed = 0;
-              }
-              else {
-                  tempTypeSpeed = (Math.random() * typeSpeed) + 50;
-              }
-              t.innerHTML += HTML[cursorPosition];
-          }
-          if (writingTag === true && HTML[cursorPosition] === ">") {
+          else {
               tempTypeSpeed = (Math.random() * typeSpeed) + 50;
-              writingTag = false;
-              if (tagOpen) {
-                  var newSpan = document.createElement("span");
-                  t.appendChild(newSpan);
-                  newSpan.innerHTML = tag;
-                  tag = newSpan.firstChild;
-              }
           }
-
-          cursorPosition += 1;
-          if (cursorPosition < HTML.length - 1) {
-              setTimeout(type, tempTypeSpeed);
+          t.innerHTML += HTML[cursorPosition];
+      }
+      if (writingTag === true && HTML[cursorPosition] === ">") {
+          tempTypeSpeed = (Math.random() * typeSpeed) + 50;
+          writingTag = false;
+          if (tagOpen) {
+              var newSpan = document.createElement("span");
+              t.appendChild(newSpan);
+              newSpan.innerHTML = tag;
+              tag = newSpan.firstChild;
           }
+      }
 
-      };
+      cursorPosition += 1;
+      if (cursorPosition < HTML.length - 1) {
+          setTimeout(type, tempTypeSpeed);
+      }
 
-      return {
-          type: type
-      };
-  }
+  };
 
-  var typer = document.getElementById('typewriter');
+  return {
+      type: type
+  };
+}
 
-  typewriter = setupTypewriter(typer);
+var typer = document.getElementById('typewriter');
 
-  typewriter.type();
+typewriter = setupTypewriter(typer);
 
-*/
+typewriter.type();
+
